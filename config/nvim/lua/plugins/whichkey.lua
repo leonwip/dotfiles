f7026a971db463b95394fd7d99c1addc9910d039
Leon <221765750+leonwip@users.noreply.github.com>
2025-08-01 03:52:01 +0200

    feat(nvim): add some whichkey icons

--- config/nvim/lua/plugins/whichkey.lua
+++ config/nvim/lua/plugins/whichkey.lua
@@ -1,5 +1,15 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    preset = "helix",
+    icons = {
+      rules = {
+        { plugin = "gitsigns.nvim", cat = "filetype", name = "git" },
+        { plugin = "neo-tree.nvim", icon = "󰙅", color = "green" },
+        { plugin = "trouble.nvim", icon = "", color = "red" },
+        { plugin = "twilight.nvim", icon = "", color = "yellow" },
+      },
+    },
+  },
 }
