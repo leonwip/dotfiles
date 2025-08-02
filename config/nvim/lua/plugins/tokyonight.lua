5a51825a1bbc60a0a7f542c36087d65897d7163e
Leon <221765750+leonwip@users.noreply.github.com>
2025-08-02 03:22:33 +0200

    feat(nvim): configure colorscheme for transparent window

--- config/nvim/lua/plugins/tokyonight.lua
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -2,7 +2,12 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
+  opts = {
+    style = "moon",
+    light_style = "day",
+    transparent = true,
+  },
   config = function()
-    vim.cmd([[colorscheme tokyonight-night]])
+    vim.cmd [[ colorscheme tokyonight ]]
   end,
 }
