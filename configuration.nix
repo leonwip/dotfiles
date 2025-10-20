86057e1752c05826f9587cad538fb4e9c7d755a0
leonwip <leonwip@proton.me>
2025-10-20 22:00:09 +0200

    little formatting and stuff

--- configuration.nix
+++ configuration.nix
@@ -1,113 +1,112 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
+    imports =[
+        ./hardware-configuration.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+    networking.hostName = "nixos";
+    networking.networkmanager.enable = true;
 
-  time.timeZone = "Europe/Berlin";
+    time.timeZone = "Europe/Berlin";
 
-  services.getty.autologinUser = "leon";
+    services.getty.autologinUser = "leon";
 
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
+    programs.hyprland = {
+        enable = true;
+        xwayland.enable = true;
+    };
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "de";
-  };
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+        font = "Lat2-Terminus16";
+        keyMap = "de";
+    };
 
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
+    services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+    };
 
-  users.users.leon = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-  };
+    users.users.leon = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+    };
 
-  environment.systemPackages = with pkgs; [
-    vim
-    cifs-utils
-  ];
+    environment.systemPackages = with pkgs; [
+        vim
+        cifs-utils
+    ];
 
-  fonts.packages = with pkgs; [
-    fira-code
-    fira-code-symbols
-    nerd-fonts.fira-code
-  ];
+    fonts.packages = with pkgs; [
+        fira-code
+        fira-code-symbols
+        nerd-fonts.fira-code
+    ];
 
-  fileSystems."/mnt/documents" = {
-    device = "//umbrella/Documents";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
-  };
-  fileSystems."/mnt/downloads" = {
-    device = "//umbrella/Downloads";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
-  };
-  fileSystems."/mnt/movies" = {
-    device = "//umbrella/Movies";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
-  };
-  fileSystems."/mnt/music" = {
-    device = "//umbrella/Music";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
-  };
-  fileSystems."/mnt/photos" = {
-    device = "//umbrella/Photos";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
-  };
-  fileSystems."/mnt/unsorted" = {
-    device = "//umbrella/Unsorted";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
-  };
+    fileSystems."/mnt/documents" = {
+        device = "//umbrella/Documents";
+        fsType = "cifs";
+        options = let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+    };
+    fileSystems."/mnt/downloads" = {
+        device = "//umbrella/Downloads";
+        fsType = "cifs";
+        options = let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+    };
+    fileSystems."/mnt/movies" = {
+        device = "//umbrella/Movies";
+        fsType = "cifs";
+        options = let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+    };
+    fileSystems."/mnt/music" = {
+        device = "//umbrella/Music";
+        fsType = "cifs";
+        options = let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+    };
+    fileSystems."/mnt/photos" = {
+        device = "//umbrella/Photos";
+        fsType = "cifs";
+        options = let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+    };
+    fileSystems."/mnt/unsorted" = {
+        device = "//umbrella/Unsorted";
+        fsType = "cifs";
+        options = let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+    };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  hardware.graphics = {
-    enable = true;
-  };
+    hardware.graphics = {
+        enable = true;
+    };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+        open = true;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
 
-  nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowUnfree = true;
 
-  system.stateVersion = "25.05";
+    system.stateVersion = "25.05";
 }
